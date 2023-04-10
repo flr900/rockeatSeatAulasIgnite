d4d94d35a933f2f05704a17b930413e1fb81f59e
flr900 <feliperodrigues9000@gmail.com>
2023-04-10 08:39:52 -0300

    Second Commit

--- Projeto01/01-Fundamentos-reactjs/src/App.jsx
+++ Projeto01/01-Fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,14 @@
+import { Header } from './components/Header'
 import {Post} from './post'
+
+import './global.css'
+
 
 function App() {
   
   return (
     <>
+    <Header></Header>
     <Post Text='Felipe' ></Post>
     </>
   )
